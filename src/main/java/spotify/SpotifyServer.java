68a72ca56d7b9827b4abdebd8a468f4dfffe8cfb
elina <valinkovae@gmail.com>
2019-07-05 00:17:13 +0300

    Added SpotifyServer file

---
+++ src/main/java/spotify/SpotifyServer.java
@@ -0,0 +1,62 @@
+package spotify;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+import spotify.service.PlaylistsRepo;
+import spotify.service.UsersRepo;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@SpringBootApplication
+@Service
+public class SpotifyServer {
+    private static final Logger log = LoggerFactory.getLogger(SpotifyServer.class);
+    static final int PORT = 4444;
+
+    private ClientProcessor clientProcessor;
+    private StateSaver stateSaver;
+
+    @Autowired
+    public SpotifyServer(UsersRepo usersRepo,
+                         PlaylistsRepo playlistsRepo,
+                         ClientProcessor clientProcessor,
+                         StateSaver stateSaver) {
+
+        this.clientProcessor = clientProcessor;
+        this.stateSaver = stateSaver;
+        stateSaver.usersFromJson(usersRepo);
+        stateSaver.playlistFromJson(playlistsRepo);
+    }
+
+    /**
+     * This method run the server
+     */
+    public void run() {
+        log.info("Starting server on port {}", PORT);
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            log.info("Successfully started server on port {}", PORT);
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept()) {
+                    log.info("Accepted socket from {}", clientSocket.getInetAddress().getHostName());
+                    clientProcessor.start();
+                } catch (IOException e) {
+                    log.error(e.getMessage(), e);
+                }
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpotifyServer.class, args);
+        applicationContext.getBean(SpotifyServer.class).run();
+    }
+}
