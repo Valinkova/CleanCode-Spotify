e0238200245e88ff83445b2b27a39df8582785c5
elina <valinkovae@gmail.com>
2019-07-05 00:14:26 +0300

    Added AppConfig file

---
+++ src/main/java/spotify/AppConfig.java
@@ -0,0 +1,42 @@
+package spotify;
+
+import org.springframework.context.annotation.Bean;
+import spotify.command.CommandName;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AppConfig {
+    private static final String LOCALHOST = "localhost";
+
+    @Bean
+    public Socket getClientSocket() throws IOException {
+        return new Socket(LOCALHOST, SpotifyServer.PORT);
+    }
+
+    @Bean({"userCommands"})
+    public Set<String> getUserCommands() {
+        Set<String> commandNames = new HashSet<>();
+        commandNames.add(CommandName.DISCONNECT.toString());
+        commandNames.add(CommandName.LOGIN.toString());
+        commandNames.add(CommandName.REGISTER.toString());
+
+        return commandNames;
+    }
+
+    @Bean({"songCommands"})
+    public Set<String> getSongCommands() {
+        EnumSet<CommandName> commands = EnumSet.allOf(CommandName.class);
+
+        return commands.stream()
+                .filter(commandName -> commandName != CommandName.DISCONNECT
+                        && commandName != CommandName.LOGIN
+                        && commandName != CommandName.REGISTER)
+                .map(CommandName::toString)
+                .collect(Collectors.toSet());
+    }
+}
